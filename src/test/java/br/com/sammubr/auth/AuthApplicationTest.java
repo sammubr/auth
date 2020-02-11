81511610e982359c0d6e74872fd4fa1b0094024f
Samuel Noswitz <samuel.noswitz@cwi.com.br>
2020-02-11 19:34:19 -0300

    Ajustes

---
+++ src/test/java/br/com/sammubr/auth/AuthApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.sammubr.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
